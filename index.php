895a3ab36a6c8411dd8c03a5c8884856ddf475c1
grzegorzhalendy <grzegorz.halendy@gmail.com>
2017-12-30 16:27:04 +0100

    Instalacja Bootstrapa

--- index.php
+++ index.php
@@ -1,16 +1,55 @@
 <!doctype html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Orkiestra Reprezentacyjna Związku Harcerstwa Polskiego Z Żurawicy</title>
-	</head>
-	<body>
-		<div id="top">
-			<header id="NAGLOWEK">Nagłówek szablonu</header>
-			<nav id="MENU">Menu nawigacyjne</nav>
-			<aside id="INFORMACJE">Dodatkowe informacje</aside>
-			<article id="TRESC">Treść strony</article>
-			<footer id="STOPKA">Stopka serwisu</footer>
-		</div>
-	</body>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Orkiestra Reprezentacyjna Związku Harcerstwa Polskiego Z Żurawicy</title>
+</head>
+
+<!--<body style="background-image: url('/img/Główne.jpg'); background-size: 100%; background-repeat: no-repeat; width: 100%">-->
+
+<body>
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel;">
+        <div class="carousel-inner">
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgba(0,0,0,0.7);">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Strona główna <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Galeria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Repertuar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Skład orkiestry</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Księga gości</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="/img/Główne.JPG" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Third slide">
+            </div>
+        </div>
+    </div>
+</body>
+
 </html>
